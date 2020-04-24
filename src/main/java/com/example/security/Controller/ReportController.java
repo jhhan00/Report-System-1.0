6efce9cb797aad5db1c2be0eec118dd8c17517e1
Lenovo <jhhan009@gmail.com>
2020-04-24 18:15:57 +0900

    fixing2

--- src/main/java/com/example/security/Controller/ReportController.java
+++ src/main/java/com/example/security/Controller/ReportController.java
@@ -47,71 +47,81 @@ public class ReportController {
     }
 
     @GetMapping("/search") // report 검색해서 보기
-    public String reportSearch(Authentication auth, Model model, @RequestParam("type")String type,
-                               @RequestParam("search")String info) {
+    public String reportSearch(Authentication auth, Model model, HttpServletRequest request) {
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority", authority);
 
+        String type = request.getParameter("search1");
+        String finding = request.getParameter("searching2");
+        log.info(type + " " + finding);
         List<Report> rlist = new ArrayList<>();
+
         if(type.equals("username")) {
-            rlist = reportRepository.findByUsernameStartsWithOrderByWriteDateDesc(info);
+            rlist = reportRepository.findByUsernameStartsWithOrderByWriteDateDesc(finding);
         } else if(type.equals("reportTitle")) {
-            rlist = reportRepository.findByReportTitleContainingOrderByWriteDateDesc(info);
+            rlist = reportRepository.findByReportTitleContainingOrderByWriteDateDesc(finding);
         } else if(type.equals("time")) {
-            log.info("Input Time : " + info);
             List<Report> rl = reportRepository.findAllByOrderByUpdatedTimeDesc();
             for(Report rp : rl) {
-                if(info.equals(rp.getWriteDate().toLocalDate().toString())) {
-                    log.info(rp.toString());
+                if(finding.equals(rp.getWriteDate().toLocalDate().toString())) {
                     rlist.add(rp);
                 }
             }
+        } else if (type.equals("type")) {
+            rlist = reportRepository.findByReportTypeOrderByWriteDateDesc(finding);
+        } else if (type.equals("state")) {
+            rlist = reportRepository.findByStateOrderByWriteDateDesc(finding);
         }
+
         model.addAttribute("list",rlist);
         return "report/report_list";
     }
 
-    @GetMapping("/sorting") // report 정렬해서 원하는 것 보기
-    public String reportSorting(Authentication auth, Model model, @RequestParam("Big")String big,
-                                @RequestParam("Small")String small) {
-        Map<String, String> authority = rd.getUserInfo(auth.getName());
-        model.addAttribute("authority", authority);
-
-        System.out.println(big + " , " + small);
-        List<Report> rlist = null;
-        if(big.equals("type")) {
-            rlist = reportRepository.findByReportTypeOrderByWriteDateDesc(small);
-        } else if(big.equals("state")) {
-            rlist = reportRepository.findByStateOrderByWriteDateDesc(small);
-        }
-        model.addAttribute("list",rlist);
-
-        return "report/report_list";
-    }
+//    @GetMapping("/sorting") // report 정렬해서 원하는 것 보기
+//    public String reportSorting(Authentication auth, Model model, @RequestParam("Big")String big,
+//                                @RequestParam("Small")String small) {
+//        Map<String, String> authority = rd.getUserInfo(auth.getName());
+//        model.addAttribute("authority", authority);
+//
+//        System.out.println(big + " , " + small);
+//        List<Report> rlist = null;
+//        if(big.equals("type")) {
+//            rlist = reportRepository.findByReportTypeOrderByWriteDateDesc(small);
+//        } else if(big.equals("state")) {
+//            rlist = reportRepository.findByStateOrderByWriteDateDesc(small);
+//        }
+//        model.addAttribute("list",rlist);
+//
+//        return "report/report_list";
+//    }
 
     @GetMapping("/detail/{reportId}") // report 상세보기
-    public String reportView(@PathVariable("reportId") long reportid, Model model, Authentication auth) {
+    public String reportView(@PathVariable("reportId") long reportid, Model model, Authentication auth,
+                             HttpServletRequest request) {
         //System.out.println(reportid + ". " + auth.getName() + " in detail");
 
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority",authority);
 
         List<Task> list = this.taskRepository.findByReportId(reportid);
+        System.out.println(list);
         model.addAttribute("list",list);
 
         Report rp = this.reportRepository.findByReportId(reportid);
         model.addAttribute("info",rp);
+        model.addAttribute("oldUrl", request.getHeader("referer"));
 
         return "report/report_detail";
     }
 
     @GetMapping("/create/daily")
-    public String createDaily(Model model, Authentication auth) {
+    public String createDaily(Model model, Authentication auth, HttpServletRequest request) {
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority", authority);
 
-        List<Map<String, Object>> list = null;
-        model.addAttribute("task",list);
+        List<Map<String, Object>> list = new ArrayList<>();
+        model.addAttribute("task", list);
+        model.addAttribute("oldUrl", request.getHeader("referer"));
 
         return "report/create_daily";
     }
@@ -165,28 +175,22 @@ public class ReportController {
     }
 
     @GetMapping("/create/weekly")
-    public String createWeekly(Authentication auth, Model model) {
+    public String createWeekly(Authentication auth, Model model, HttpServletRequest request) {
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority", authority);
 
         List<Report> rlist = reportRepository.findByReportTypeAndUsername("Weekly",auth.getName());
         long idx = -1;
-        if(rlist.size() != 0){
-//            System.out.println(rlist);
-//            System.out.println(rlist.get(0).getReportId());
-//            System.out.println(rlist.get(rlist.size()-1).getReportId());
+        boolean isNull = true; // 아무 것도 없으면 처음 주간 리포트 쓰는 경우
+        if(rlist.size() != 0) {
             idx = rlist.get(rlist.size()-1).getReportId();
+            isNull = false; // 있다면 주간 리포트를 한 번 이상 쓴 경우
         }
         List<Task> tlist = null;
         if(idx != -1) tlist = taskRepository.findByReportId(idx);
         model.addAttribute("task", tlist);
-
-        boolean isValid = false;
-        System.out.println("idx : " + idx);
-//        if(tlist != null)System.out.println(tlist.get(tlist.size()-1));
-        if(tlist != null && tlist.get(tlist.size()-1).getReportKind().equals("weekly_plan")) isValid=true;
-        System.out.println("isValid : " + isValid);
-        model.addAttribute("Valid",isValid);
+        model.addAttribute("Valid", isNull);
+        model.addAttribute("oldUrl", request.getHeader("referer"));
 
         return "report/create_weekly";
     }
@@ -194,41 +198,21 @@ public class ReportController {
     @PostMapping("/create/weekly")
     public String createWeeklyAction(Authentication auth, HttpServletRequest request) {
         Enumeration<String> keys = request.getParameterNames();
-        String line = keys.nextElement();
-        String temp = "";
-        if(request.getParameter(line).equals("plan"))
-            temp = "weekly_plan";
-        else if(request.getParameter(line).equals("after"))
-            temp = "weekly_result";
-
-        List<Report> rlist = reportRepository.findByReportTypeAndUsername("Weekly",auth.getName());
-        long idx = -1;
-        if(rlist.size() != 0) idx = rlist.get(rlist.size()-1).getReportId();
-        System.out.println("idx = " + idx);
 
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
         String nowDate = now.format(dtf);
 
         //Report Save
-        Report report;
-        if(temp.equals("weekly_plan")){
-            report = new Report();
-            report.setUsername(auth.getName());
-            report.setReportType("Weekly");
-            report.setSimpleDate(nowDate);
-            report.setWriteDate(now);
-            report.setUpdatedTime(now);
-            report.setState("Waiting");
-            report.setReportTitle(nowDate + "_Weekly_Report");
-            System.out.println(report);
-        } else {
-            report = reportRepository.findByReportId(idx);
-            report.setUpdatedTime(now);
-            report.setState("Waiting");
-            report.setReportTitle(nowDate + "_Weekly_Report");
-            System.out.println(report);
-        }
+        Report report = new Report();
+        report.setUsername(auth.getName());
+        report.setReportType("Weekly");
+        report.setSimpleDate(nowDate);
+        report.setWriteDate(now);
+        report.setUpdatedTime(now);
+        report.setState("Waiting");
+        report.setReportTitle(nowDate + "_Weekly_Report");
+        System.out.println(report);
         reportRepository.save(report);
 
         System.out.println(report.getReportId());
@@ -236,27 +220,24 @@ public class ReportController {
 
         while(keys.hasMoreElements()) {
             String key = keys.nextElement();
-            log.info(key + " : " + request.getParameter(key));
+            int loc1 = key.indexOf("done");
+            int loc2 = key.indexOf("plan");
+            log.info(key + " : " + request.getParameter(key) + " -- loc1 : " + loc1 + ", loc2 : " + loc2);
             Task task = new Task();
             task.setReportId(r_id);
             task.setUsername(auth.getName());
             task.setSimpleDate(nowDate);
             task.setReportType("Weekly");
-            task.setReportKind(temp);
-            if(temp.equals("weekly_plan")) { // Weekly Report의 시작인 경우
+            if(loc1 == 0) {
+                task.setReportKind("weekly_result");
+                task.setDone(request.getParameter(key));
+                key = keys.nextElement();
+                task.setRealAchievement(request.getParameter(key));
+            } else if(loc2 == 0) {
+                task.setReportKind("weekly_plan");
                 task.setProgress(request.getParameter(key));
                 key = keys.nextElement();
                 task.setExpectedAchievement(request.getParameter(key));
-            } else { // Weekly Report의 끝인 경우
-                task.setDone(request.getParameter(key));
-                Task t1 = taskRepository.findByReportIdAndProgress(r_id,task.getDone());
-                System.out.println("t1 : " + t1);
-                if(t1 != null) {
-                    System.out.println("Right Now ExpectedAchievement : " + t1.getExpectedAchievement());
-                    task.setExpectedAchievement(t1.getExpectedAchievement());
-                }
-                key = keys.nextElement();
-                task.setRealAchievement(request.getParameter(key));
             }
             key = keys.nextElement();
             //
@@ -274,7 +255,7 @@ public class ReportController {
     }
 
     @GetMapping("/create/monthly")
-    public String createMonthly(Authentication auth, Model model) {
+    public String createMonthly(Authentication auth, Model model, HttpServletRequest request) {
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority", authority);
 
@@ -291,6 +272,7 @@ public class ReportController {
         if(idx != -1) tlist = taskRepository.findByReportId(idx);
         model.addAttribute("task",tlist);
         model.addAttribute("boolValue",isNull);
+        model.addAttribute("oldUrl", request.getHeader("referer"));
 
         return "report/create_monthly";
     }
@@ -355,12 +337,13 @@ public class ReportController {
     }
 
     @GetMapping("/create/project_goal")
-    public String createProjectGoal(Authentication auth, Model model) {
+    public String createProjectGoal(Authentication auth, Model model, HttpServletRequest request) {
         Map<String, String> authority = rd.getUserInfo(auth.getName());
         model.addAttribute("authority", authority);
 
         List<Task> tlist = null;
         model.addAttribute("task",tlist);
+        model.addAttribute("oldUrl", request.getHeader("referer"));
 
         return "report/create_yearly";
     }
